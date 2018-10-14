24486f24294d694b9d33823b9a389253129a2fc4
George Li <li.6795@osu.edu>
2018-10-14 15:13:13 -0400

    added multiple datapoints

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from sklearn.manifold import MDS
+import numpy as np
 import json
 import pyemblib
 
@@ -10,9 +12,14 @@ embedding_file = 'test.txt' # change file here
 @app.route('/index')
 def loadEmbeddings():
   embeddings = pyemblib.read(embedding_file, mode=pyemblib.Mode.Text)
-  encoded = {}
-  for k in embeddings:
-    encoded[str(k)] = embeddings[k].tolist()
-  return render_template('index.html', data=encoded)
+  keys = embeddings.keys()
+  values = np.array(embeddings.values())
+  mds = MDS(n_components=3)
+  values_transformed = mds.fit_transform(values)
+
+  transformed = {}
+  for i in range(len(keys)):
+    transformed[keys[i]] = values_transformed[i].tolist()
+  return render_template('index.html', data=transformed)
 
 
