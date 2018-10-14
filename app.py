1a8c808a149cb828869bba5c2d5bef5d5ae718f1
Brendan Whitaker <whitaker.213@osu.edu>
2018-10-14 11:44:19 -0400

    more crap

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ embedding_file = 'top_10000_emb.txt' # change file here
 @app.route('/index')
 def loadEmbeddings():
   embeddings = pyemblib.read(embedding_file, mode=pyemblib.Mode.Text)
-  return render_template('index.html')#, data=json.dumps(embeddings))
+  for k in embeddings:
+    embeddings[str(k)] = embeddings[k].tolist()
+    del k
+  return render_template('index.html', data=json.dumps(embeddings))
 
 
